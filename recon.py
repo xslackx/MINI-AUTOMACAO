bab6426d6c724fc3856b49577712593194e986bb
Ruan <xslackx@live.fr>
2024-04-18 12:01:31 -0300

    add findomain

--- recon.py
+++ recon.py
@@ -18,18 +18,25 @@ class Recon(InfoRecon):
         
         try:
             if urlparse(domainame):
-                self.dominio = gethostbyname(urlparse(domainame).netloc)
+                self.iphost = gethostbyname(urlparse(domainame).netloc)
+                self.domainame = domainame
         except:
             raise
         
         super().__init__()
     
     def automata(self, nmap: bool, domain: bool, arjun: bool):
-        if domain: pass
+
+        if domain:
+            cmd = "findomain -q -t " + self.domainame
+            with Popen(cmd, shell=True, stdout=PIPE) as finddomain:
+                response = finddomain.stdout.read()
+                self.restools["findomain"] = response.decode()
+                
         if arjun: pass
         
         if nmap:
-            cmd = "nmap -p 21,22,25,53,80,110,123,143,443,465,631,993,995 " + self.dominio
+            cmd = "nmap -p 21,22,25,53,80,110,123,143,443,465,631,993,995 " + self.iphost
             with Popen(cmd, shell=True, stdout=PIPE) as netmap:
                 response = netmap.stdout.read()
                 response = response.decode().split("\n\n")
